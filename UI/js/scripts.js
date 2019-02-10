d31dbe67b755dc79b360dd6c0b7362b1e1dc0451
Patrick Ngabonziza <ngabonzizap7@gmail.com>
2019-02-10 15:22:04 +0200

    restrutured pages

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -1,3 +1,21 @@
-function myFunction() { 
-  document.getElementById("offideEditModal").showModal(); 
+const modal = document.getElementById('edit-office-modal');
+const edit_button = document.getElementById('edit-office');
+
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+edit_button.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "block";
+  }
 }
